3102d1a7b06c822d10cb5d5af19c6757b61b506a
Matheus Reis <matheus.reis081@gmail.com>
2023-01-25 11:49:25 -0300

    feat: binding variable in notes adapter

--- app/src/main/java/com/matheusxreis/notes/adapters/NotesAdapter.kt
+++ app/src/main/java/com/matheusxreis/notes/adapters/NotesAdapter.kt
@@ -1,21 +1,26 @@
 package com.matheusxreis.notes.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.matheusxreis.notes.databinding.NotesRowLayoutBinding
 import com.matheusxreis.notes.models.Note
 
 class NotesAdapter():RecyclerView.Adapter<NotesAdapter.NotesViewHolder>(){
 
     var notes:List<Note> = listOf()
 
-    class NotesViewHolder(parent: ViewGroup):RecyclerView.ViewHolder(parent) {
+    class NotesViewHolder(private val binding: NotesRowLayoutBinding):RecyclerView.ViewHolder(binding.root) {
         fun bind(note: Note){
-
+            binding.note = note
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesAdapter.NotesViewHolder {
-       return NotesViewHolder(parent)
+
+      val layoutInflater = LayoutInflater.from(parent.context)
+      val binding = NotesRowLayoutBinding.inflate(layoutInflater, parent, false)
+       return NotesViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: NotesAdapter.NotesViewHolder, position: Int) {
