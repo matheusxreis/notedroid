bdb2f24e388b0d264fb12871f75bc5f8f095cee6
Matheus Reis <matheus.reis081@gmail.com>
2023-01-25 09:55:05 -0300

    feat: setted up app bar and nav controller in main activity

--- app/src/main/java/com/matheusxreis/notes/MainActivity.kt
+++ app/src/main/java/com/matheusxreis/notes/MainActivity.kt
@@ -2,10 +2,27 @@ package com.matheusxreis.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.my_nav_host)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.notes_fragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
