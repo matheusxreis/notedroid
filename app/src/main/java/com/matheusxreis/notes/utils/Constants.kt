117e50f32633325976d257fb4582801c901dce7d
Matheus Reis <matheus.reis081@gmail.com>
2023-01-25 15:27:54 -0300

    feat: added room constants

--- app/src/main/java/com/matheusxreis/notes/utils/Constants.kt
+++ app/src/main/java/com/matheusxreis/notes/utils/Constants.kt
@@ -10,5 +10,10 @@ class Constants {
         val FILTER_IMPORTANT_ID_KEY="filter_important_id"
         val FILTER_IMPORTANT_VALUE_DEFAULT = "filter_important_name_value_default"
 
+
+        // room
+
+        val DATABASE_NAME="notes_database"
+       const val TABLE_NAME_NOTE="notes"
     }
 }
