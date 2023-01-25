3945ad84eb1a2afdc90308b4709267a4dedaa8af
Matheus Reis <matheus.reis081@gmail.com>
2023-01-25 14:15:16 -0300

    feat: created constansts class

---
+++ app/src/main/java/com/matheusxreis/notes/utils/Constants.kt
@@ -0,0 +1,14 @@
+package com.matheusxreis.notes.utils
+
+class Constants {
+
+    companion object {
+
+        // datastore
+        val DATASTORE_NAME = "datastore_important_filter_preference"
+        val FILTER_IMPORTANT_NAME_KEY="filter_important_id"
+        val FILTER_IMPORTANT_ID_KEY="filter_important_id"
+        val FILTER_IMPORTANT_VALUE_DEFAULT = "filter_important_name_value_default"
+
+    }
+}
