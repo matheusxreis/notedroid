66e6b6853f79478e72ca7e52898af7afc1b3cf30
Matheus Reis <matheus.reis081@gmail.com>
2023-01-25 12:10:16 -0300

    feat: apply main view model in notes fragment

--- app/src/main/java/com/matheusxreis/notes/ui/fragments/NotesFragment.kt
+++ app/src/main/java/com/matheusxreis/notes/ui/fragments/NotesFragment.kt
@@ -5,22 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.matheusxreis.notes.R
 import com.matheusxreis.notes.adapters.NotesAdapter
 import com.matheusxreis.notes.models.Note
+import com.matheusxreis.notes.viewmodels.MainViewModel
 import kotlinx.android.synthetic.main.fragment_notes.view.*
 
 class NotesFragment : Fragment() {
 
     private lateinit var notesAdapter: NotesAdapter;
     private lateinit var mView: View;
+    private lateinit var mainViewModel: MainViewModel;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         notesAdapter = NotesAdapter()
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
 
     }
 
@@ -43,14 +47,10 @@ class NotesFragment : Fragment() {
     }
 
     fun populateRecyclerView() {
-        val mockListNotes = listOf(
-            Note(id=1, title="Be or not be", text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam et sem a maximus. Integer eleifend nisi a aliquet posuere. Nulla mollis facilisis enim, nec malesuada libero consequat at. Curabitur lacinia pulvinar ultricies. Mauris vel feugiat leo. Sed at placerat nulla, ac maximus velit. Ut aliquet dignissim dolor finibus aliquet."),
-            Note(id=2, title="Be or not be", text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam et sem a maximus. Integer eleifend nisi a aliquet posuere. Nulla mollis facilisis enim, nec malesuada libero consequat at. Curabitur lacinia pulvinar ultricies. Mauris vel feugiat leo. Sed at placerat nulla, ac maximus velit. Ut aliquet dignissim dolor finibus aliquet."),
-            Note(id=3, title="Be or not be", text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam et sem a maximus. Integer eleifend nisi a aliquet posuere. Nulla mollis facilisis enim, nec malesuada libero consequat at. Curabitur lacinia pulvinar ultricies. Mauris vel feugiat leo. Sed at placerat nulla, ac maximus velit. Ut aliquet dignissim dolor finibus aliquet."),
-            Note(id=4, title="Be or not be", text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam et sem a maximus. Integer eleifend nisi a aliquet posuere. Nulla mollis facilisis enim, nec malesuada libero consequat at. Curabitur lacinia pulvinar ultricies. Mauris vel feugiat leo. Sed at placerat nulla, ac maximus velit. Ut aliquet dignissim dolor finibus aliquet."),
-            Note(id=5, title="Be or not be", text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam et sem a maximus. Integer eleifend nisi a aliquet posuere. Nulla mollis facilisis enim, nec malesuada libero consequat at. Curabitur lacinia pulvinar ultricies. Mauris vel feugiat leo. Sed at placerat nulla, ac maximus velit. Ut aliquet dignissim dolor finibus aliquet."),
-            Note(id=6, title="Be or not be", text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam et sem a maximus. Integer eleifend nisi a aliquet posuere. Nulla mollis facilisis enim, nec malesuada libero consequat at. Curabitur lacinia pulvinar ultricies. Mauris vel feugiat leo. Sed at placerat nulla, ac maximus velit. Ut aliquet dignissim dolor finibus aliquet."),
-            )
-        notesAdapter.setData(mockListNotes)
+        mainViewModel.getNotes()
+
+        mainViewModel.notes.observe(viewLifecycleOwner) {
+            notesAdapter.setData(it)
+        }
     }
 }
