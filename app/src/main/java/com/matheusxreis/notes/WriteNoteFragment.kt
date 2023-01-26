201f0ad4b3839a058dad1745d9a8befb1c63eaa9
Matheus Reis <matheus.reis081@gmail.com>
2023-01-26 09:07:26 -0300

    feat: added onbackpressedcallback in write note fragment

--- app/src/main/java/com/matheusxreis/notes/WriteNoteFragment.kt
+++ app/src/main/java/com/matheusxreis/notes/WriteNoteFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
 
 class WriteNoteFragment : Fragment() {
 
@@ -12,6 +14,13 @@ class WriteNoteFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        class Back(enabled: Boolean) : OnBackPressedCallback(enabled) {
+            override fun handleOnBackPressed() {
+               findNavController().navigate(R.id.action_writeNoteFragment_to_notes_fragment)
+            }
+
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, Back(true))
     }
 
     override fun onCreateView(
