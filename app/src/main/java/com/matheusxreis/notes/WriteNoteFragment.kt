d72b59d0d8a34683a660c5f5e212d84120cdbe9a
Matheus Reis <matheus.reis081@gmail.com>
2023-01-26 09:14:41 -0300

    feat: binding write note fragment

--- app/src/main/java/com/matheusxreis/notes/WriteNoteFragment.kt
+++ app/src/main/java/com/matheusxreis/notes/WriteNoteFragment.kt
@@ -7,9 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.navigation.fragment.findNavController
+import com.matheusxreis.notes.databinding.FragmentWriteNoteBinding
+import com.matheusxreis.notes.models.Note
 
 class WriteNoteFragment : Fragment() {
 
+    lateinit var binding:FragmentWriteNoteBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +30,9 @@ class WriteNoteFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_write_note, container, false)
+
+        binding = FragmentWriteNoteBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
